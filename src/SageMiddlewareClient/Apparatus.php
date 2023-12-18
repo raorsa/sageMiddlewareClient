0f695d99b67dc495a11673efc3a5f5e377b7f2c5
josevi <jvcanet@raorsa.>
2023-12-18 12:33:26 +0100

    add apparatus support

---
+++ src/SageMiddlewareClient/Apparatus.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Raorsa\SageMiddlewareClient;
+
+class Apparatus extends GenericClient
+{
+    const BASE = 'apparatus';
+
+    public function list(string $client)
+    {
+        return $this->callJson(self::BASE . '/' . $client);
+    }
+
+    public function interventions(string $id)
+    {
+        return $this->callJson(self::BASE . '/' . $id . '/interventions');
+    }
+
+    public function brands()
+    {
+        return $this->callJson(self::BASE . '/brands');
+    }
+
+    public function types()
+    {
+        return $this->callJson(self::BASE . '/types');
+    }
+
+    public function warranties()
+    {
+        return $this->callJson(self::BASE . '/warranties');
+    }
+
+    public function interventionsType()
+    {
+        return $this->callJson(self::BASE . '/interventions-type');
+    }
+}
