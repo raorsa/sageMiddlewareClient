0bdf4e62fd7c3df4f767225ef89f808831064adb
jsvcanet <jvcanet@raorsa.es>
2025-01-21 13:04:44 +0100

    fix return string to object

--- src/SageMiddlewareClient/DeliveryNotes.php
+++ src/SageMiddlewareClient/DeliveryNotes.php
@@ -18,7 +18,7 @@ class DeliveryNotes extends baseClient
         return $this->callJson(self::BASE . '/info/' . $id, $cache);
     }
 
-    public function emptyLines(string $year, bool $cache = true): string|false
+    public function emptyLines(string $year, bool $cache = true): object|false
     {
         return $this->callJson(self::BASE . '/empty-lines/' . $year, $cache);
     }
